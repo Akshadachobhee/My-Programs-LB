8c4ec3947b6777edd1adda522daebb2b294734b9
Akshadachobhee <akshadachobhe7@gmail.com>
2023-04-26 22:12:17 +0530

    Print 1 to 5 number (Sequence)
    Sequence

---
+++ C LB/program15.c
@@ -0,0 +1,12 @@
+// Print 1 to 5 number
+#include<stdio.h>
+ 
+ int main()
+ {
+    printf("1\n");
+    printf("2\n");
+    printf("3\n");
+    printf("4\n");
+    printf("5\n");
+     return 0;
+ }
