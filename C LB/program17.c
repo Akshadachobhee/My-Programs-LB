46bbebc2184a69224794f77dbe9594a79fe9fe56
Akshadachobhee <akshadachobhe7@gmail.com>
2023-04-26 22:14:22 +0530

    Print 1 to 5 number (Adding Function)
    Using Business Logic format

---
+++ C LB/program17.c
@@ -0,0 +1,16 @@
+// Print 1 to 5 number
+#include<stdio.h>
+ 
+ void Display()
+ {
+     int iCnt = 0;
+    for(iCnt = 1; iCnt <= 5 ; iCnt++)
+    {
+        printf("%d\n",iCnt);
+    }
+ }
+ int main()
+ {
+   Display();
+     return 0;
+ }
