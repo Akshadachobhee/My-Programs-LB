dc83e712149f8ac8d588803f6c600613141a02df
Akshadachobhee <akshadachobhe7@gmail.com>
2023-04-26 22:06:25 +0530

    Display "Jay ganesh" 5 times on screen
    It's just a sequence program

---
+++ C LB/program9.c
@@ -0,0 +1,14 @@
+//Display "Jay ganesh" 5 times on screen 
+
+#include<stdio.h>
+int main()
+{
+// Example for sequence
+    printf("Jay Ganesh ...\n");
+    printf("Jay Ganesh ...\n");
+    printf("Jay Ganesh ...\n");
+    printf("Jay Ganesh ...\n");
+    printf("Jay Ganesh ...\n");
+
+    return 0;
+}
