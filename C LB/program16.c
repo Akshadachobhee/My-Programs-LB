e73f5edf20c6d9d656a473354b09481529f6111e
Akshadachobhee <akshadachobhe7@gmail.com>
2023-04-26 22:13:17 +0530

    Print 1 to 5 number (iteration)
    Using for loop

---
+++ C LB/program16.c
@@ -0,0 +1,12 @@
+// Print 1 to 5 number
+#include<stdio.h>
+ 
+ int main()
+ {
+   int iCnt = 0;
+    for(iCnt = 1; iCnt <= 5 ; iCnt++)
+    {
+        printf("%d\n",iCnt);
+    }
+     return 0;
+ }
