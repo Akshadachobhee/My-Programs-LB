8ed0561fb04d1fdf772f2bdbb2023187b3747488
Akshadachobhee <akshadachobhe7@gmail.com>
2023-06-23 23:38:21 +0530

    Create program10.c

---
+++ C LB/program10.c
@@ -0,0 +1,13 @@
+//Display "Jay ganesh" 5 times on screen 
+
+#include<stdio.h>
+int main()
+{
+    //Using for 
+    int iCnt = 0;
+    for(iCnt = 1 ; iCnt <= 5 ; iCnt++ )
+    {
+        printf("Jay Ganesh...\n");
+    }
+    return 0;
+}
