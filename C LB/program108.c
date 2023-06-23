7ef724a7dbfb76f8bcfd59b10f81e367c0b67bea
Akshadachobhee <akshadachobhe7@gmail.com>
2023-06-23 23:39:35 +0530

    Create program108.c

---
+++ C LB/program108.c
@@ -0,0 +1,32 @@
+/*
+Input :  1 2 3 4 5 4 3 2 1
+Output : 
+
+*/
+
+#include<stdio.h>
+ void Display(int iFreq)
+ {  
+    int iCnt = 0;
+  
+    for(iCnt = 1; iCnt <= iFreq;iCnt++)
+    {
+    printf("%d\t",iCnt); //4
+    }
+    for (iCnt = iFreq-1; iCnt >= 1;iCnt--)
+    {
+      printf("%d\t",iCnt); 
+    }
+    
+    printf("\n");
+ } 
+ int main()
+ {
+      int iFrequency = 0;
+    printf("Enter the frequency of symbol : ");
+    scanf("%d",&iFrequency);
+ 
+    Display(iFrequency);
+
+    return 0;
+ }
