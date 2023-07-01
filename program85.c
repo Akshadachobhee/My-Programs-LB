f0023473f433849417ab63b9bff93713496a523a
Akshada <102922884+Akshadachobhee@users.noreply.github.com>
2023-07-01 23:12:31 +0530

    Add files via upload

---
+++ program85.c
@@ -0,0 +1,61 @@
+//Accpet numbers and display the largest number
+//time Complexity is 2n as n for minimum and n for maximum
+#include<stdio.h>
+#include<stdlib.h>
+
+int Maximum(int Arr[], int iLength)
+{
+    int iMax = Arr[0];
+    int iCnt = 0;
+    for(iCnt= 0; iCnt < iLength; iCnt++)
+    {
+        if(Arr[iCnt] > iMax)
+        {
+            iMax = Arr[iCnt];
+        }
+    }
+    return iMax;
+}
+
+int Minimum(int Arr[], int iLength)
+{
+    int iMin = Arr[0];
+    int iCnt = 0;
+    for(iCnt= 0; iCnt < iLength; iCnt++)
+    {
+        if(Arr[iCnt] <iMin)
+        {
+            iMin = Arr[iCnt];
+        }
+    }
+    return iMin;
+}
+
+int main()
+{
+    int iSize = 0;
+    int *ptr = NULL;
+    int iCnt = 0;
+    int iRet = 0;
+    printf("Enter number of elements:\n");
+    scanf("%d",&iSize);
+    ptr = (int *) malloc(iSize * sizeof(int));
+    printf("Enter the elements:\n");
+    for(iCnt = 0; iCnt<iSize; iCnt++)
+    {
+        scanf("%d",&ptr[iCnt]);
+    }
+    printf("Elements of the array are:\n");
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        printf("%d\n",ptr[iCnt]);
+    }
+    iRet = Maximum(ptr,iSize);
+
+    printf("Largest number is:%d\n",iRet);
+
+    iRet = Minimum(ptr,iSize);
+    printf("Smallest number is:%d\n",iRet);
+    free(ptr);
+    return 0;
+}
