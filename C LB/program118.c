5400a97c7dae01f7441e007cd4d46f3679bfea0b
Akshadachobhee <akshadachobhe7@gmail.com>
2023-07-03 22:18:20 +0530

    Create program118.c

---
+++ C LB/program118.c
@@ -0,0 +1,43 @@
+/*
+Input :  
+Rows : 3
+col:   5
+Output : 
+a b c d e
+a b c d e
+a b c d e
+
+*/
+
+#include<stdio.h>
+ void Display(int iRow, int iCol)
+ {  
+   int i = 0;
+   int j = 0;
+    char ch = '\0';
+   for(i=1;i<= iRow;i++) // outer loop
+   {
+    for(j=1,ch ='a'; j<=iCol ; j++,ch++) // Inner loop
+    {
+        printf("%c\t",ch);
+                 
+    }
+    printf("\n");
+    
+   }
+ } 
+ int main()
+ {
+     int iValue1 = 0;
+     int iValue2 = 0;
+
+      printf("Enter rows : \n");
+      scanf("%d",&iValue1);
+
+      printf("Enter columns : \n");
+      scanf("%d",&iValue2);
+
+      Display(iValue1, iValue2);
+
+      return 0;
+ }
