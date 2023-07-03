1b0c6a0093ebda0dc30bb9eb39de510ee1a79734
Akshadachobhee <akshadachobhe7@gmail.com>
2023-07-03 22:20:17 +0530

    Create program123.c

---
+++ C LB/program123.c
@@ -0,0 +1,47 @@
+/*
+Input :  
+Rows : 4
+col:   4
+Output : 
+1 & 3 &  
+1 & 3 & 
+1 & 3 & 
+1 & 3 & 
+
+*/
+
+#include<stdio.h>
+ void Display(int iRow, int iCol)
+ {  
+   int i = 0;
+   int j = 0;
+ 
+   for(i=1;i<= iRow;i++) // outer loop
+   {
+    for(j=1; j<=iCol ; j++) // Inner loop
+    {
+         if((j%2)!=0) // Column nusar hot
+         {
+         printf("%d\t&\t",j);
+         }  
+                 
+    }
+    printf("\n");
+    
+   }
+ } 
+ int main()
+ {
+     int iValue1 = 0;
+     int iValue2 = 0;
+
+      printf("Enter rows : \n");
+      scanf("%d",&iValue1);
+
+      printf("Enter columns : \n");
+      scanf("%d",&iValue2);
+
+      Display(iValue1, iValue2);
+
+      return 0;
+ }
