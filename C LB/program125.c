70c074e9e5739e044a0631d194d6abefb9511e46
Akshadachobhee <akshadachobhe7@gmail.com>
2023-07-03 22:20:47 +0530

    Create program125.c

---
+++ C LB/program125.c
@@ -0,0 +1,56 @@
+/*
+Input :  
+Rows : 4
+col:   4
+Output : 
+# $ $ $
+$ # $ $
+$ $ # $
+$ $ $ #
+*/
+
+#include<stdio.h>
+ void Display(int iRow, int iCol)
+ {  
+   int i = 0;
+   int j = 0;
+
+   if(iRow != iCol)
+   {
+     printf("Inavalid input\n");
+   }
+  else{
+   for(i=1;i<= iRow;i++) // outer loop
+   {
+    for(j=1; j<=iCol ; j++) // Inner loop
+    {
+         if(i==j) // Column nusar hot
+         {
+         printf("#\t");
+         }  
+         else
+         {
+          printf("$\t");
+         }
+                 
+    }
+    printf("\n");
+    
+   }
+  }
+ } 
+ int main()
+ {
+     int iValue1 = 0;
+     int iValue2 = 0;
+
+      printf("Enter rows : \n");
+      scanf("%d",&iValue1);
+
+      printf("Enter columns : \n");
+      scanf("%d",&iValue2);
+
+      Display(iValue1, iValue2);
+
+      return 0;
+ }
