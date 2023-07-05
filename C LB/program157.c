96184c2325738d376387750d1f15b19fe3ff45df
Akshadachobhee <akshadachobhe7@gmail.com>
2023-07-05 22:08:16 +0530

    Create program157.c

---
+++ C LB/program157.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int strlenSmallX(char *str)
+{
+    int iCnt =0 ;
+    while()
+}
+int main()
+{
+    char Arr[20]; //Static array
+
+    printf("Enter your name : \n");
+    scanf("%[^'\n']s",Arr); //%[^'\n']s = Regular Expression for accepting space
+
+    printf("Your name is : %s\n",Arr);
+    
+    return 0;
+
+    }
