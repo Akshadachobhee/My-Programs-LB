d7e00d832362d7292f759b29df342fc1891a8c81
Akshadachobhee <akshadachobhe7@gmail.com>
2023-07-07 23:21:20 +0530

    Create program198.c

---
+++ C LB/program198.c
@@ -0,0 +1,42 @@
+// String comparison
+//wwithout if 
+//without if else
+// converting while into for
+//for loop without body
+#include<stdio.h>
+#include<stdbool.h>
+
+bool strcmpX(char *str1, char *str2)
+{
+    for(;((*str1 !='\0') && (*str2 !='\0') && (*str1 == *str2));str1++,str2++); // ; IS important
+    
+   // without if else
+  return((*str1 == '\0') && (*str2 == '\0'));
+
+}
+
+int main()
+{
+    
+    char Arr[20];
+    char Brr[20];
+    bool bRet = false;
+
+    printf("Enter first String : \n");
+    scanf("%[^'\n']s",Arr);
+
+    printf("Enter Second String : \n");
+    scanf("%[^'\n']s",Brr);
+    
+    bRet = strcmpX(Arr,Brr);
+    
+    if (bRet==true)
+    {
+        printf("Both the strings are identical.\n");
+    }
+    else
+    {
+        printf("Strings are different. \n");
+    }
+    return 0;
+}
