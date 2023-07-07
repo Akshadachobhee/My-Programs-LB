18590264db199bd5fdd5db673f3e2273d3e05c11
Akshadachobhee <akshadachobhe7@gmail.com>
2023-07-07 23:21:16 +0530

    Create program113.c

---
+++ C LB/program113.c
@@ -0,0 +1,42 @@
+// Non-linear
+//
+/*
+Input :  
+Rows : 3
+col:   5
+Output : 
+ * * * * *
+ * * * * *
+ * * * * *
+*/
+
+#include<stdio.h>
+ void Display(int iRow, int iCol)
+ {  
+   int i = 0;
+   int j = 0;
+
+   for(i=1;i<= iRow;i++) // outer loop
+   {
+    for(j=1; j<=iCol ; j++) // Inner loop
+    {
+        printf("*\t");
+    }
+    printf("\n");
+   }
+ } 
+ int main()
+ {
+     int iValue1 = 0;
+     int iValue2 = 0;
+
+      printf("Enter rows : \n");
+      scanf("%d",&iValue1);
+
+      printf("Enter columns : \n");
+      scanf("%d",&iValue2);
+
+      Display(iValue1, iValue2);
+
+      return 0;
+ }
