ad05df1e7b184ccc1dec81d6b073cf3591487425
Akshadachobhee <akshadachobhe7@gmail.com>
2023-07-07 23:21:50 +0530

    Create program140.c

---
+++ C LB/program140.c
@@ -0,0 +1,45 @@
+/*
+Input :
+Rows : 5
+col:   5
+Output :
+*
+
+*/
+
+#include<stdio.h>
+ void Display(int iRow, int iCol)
+ {  
+   int i = 0;
+   int j = 0;
+   char ch = '\0';
+  if(iRow != iCol)
+   {
+     printf("Inavalid input\n");
+   }
+   // for loop madhe optimized kel ... siranha program137.c wala code
+   for(i=1;i<= iRow;i++) // outer loop
+   {
+    for(j=1; ch = 'A' , j<=i; j++,ch++) // Inner loop 
+    {
+            printf("%c\t",ch);     
+    }
+    printf("\n");
+   }
+    
+ } 
+ int main()
+ {
+     int iValue1 = 0;
+     int iValue2 = 0;
+
+      printf("Enter rows : \n");
+      scanf("%d",&iValue1);
+
+      printf("Enter columns : \n");
+      scanf("%d",&iValue2);
+
+      Display(iValue1, iValue2);
+
+      return 0;
+ }
