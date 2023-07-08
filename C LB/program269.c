e77e69e49d6a053f5aa8e02685ac812812a179ea
Akshadachobhee <akshadachobhe7@gmail.com>
2023-07-08 21:45:12 +0530

    Create program269.c

---
+++ C LB/program269.c
@@ -0,0 +1,18 @@
+//Template for Doubly Linked List
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    struct Node *prev; //#
+    
+} NODE,*PNODE,**PPNODE;
+
+int main()
+{
+    PNODE First = NULL;
+
+    return 0;
+}
