07d2ccd702c0079f3514ea3891cf90a49990812a
Akshadachobhee <akshadachobhe7@gmail.com>
2023-07-10 22:44:41 +0530

    Create program21.c

---
+++ C LB/program21.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void DisplayFactores(int iNo)
+{
+
+}
+int main()
+{
+    int iValue = 0;
+    printf("Enter number\n");
+    scanf("%d",&iValue);
+    DisplayFactores(iValue);
+    return 0;
+}
