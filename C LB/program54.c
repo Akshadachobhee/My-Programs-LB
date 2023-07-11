4e1c94984c82072cfd09c43b13964579aa080a96
Akshadachobhee <akshadachobhe7@gmail.com>
2023-07-11 22:33:48 +0530

    Create program54.c

---
+++ C LB/program54.c
@@ -0,0 +1,46 @@
+
+#include<stdio.h>
+#include<stdbool.h>
+
+bool CheckDigit(int iNo)
+{
+int iDigit = 0;
+
+while(iNo != 0)
+{
+    iDigit = iNo % 10;
+    if(iDigit == 8)
+    {
+        break;
+    }
+    iNo = iNo / 10;
+}
+if(iDigit == 8)
+{
+    return true;
+}
+else
+{
+    return false;
+}
+}
+int main()
+{ 
+    int iValue = 0;
+    bool bRet = false;
+
+    printf("Enter number : \n");
+    scanf("%d",&iValue);
+    
+    bRet = CheckDigit(iValue);
+    if(bRet == false)
+    {
+        printf("Digit 8 is present");
+    }
+    else
+    {
+        printf("Digit 8 is not present");
+    }
+    
+    return 0;
+}
