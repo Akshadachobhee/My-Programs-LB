679eb5e3137ac771bddd29e0eed4927dcc3809b4
Akshadachobhee <akshadachobhe7@gmail.com>
2023-07-12 23:02:12 +0530

    Create program73.c

---
+++ C LB/program73.c
@@ -0,0 +1,22 @@
+// Static memory allocation
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int Arr[5];
+    int iCnt = 0; 
+    printf("Enter the elements : \n");
+    for(iCnt =0 ; iCnt < 5 ; iCnt++)
+    {
+    scanf("%d",&Arr[iCnt]);
+    }
+
+    printf("Elements of array are : \n");
+    for ( iCnt = 0; iCnt < 5; iCnt++) 
+    {
+    printf("%d\n",Arr[iCnt]);
+    }
+    return 0;
+}
