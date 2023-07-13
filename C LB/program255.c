3be241ad3908138e976d7ed5cc2d8ded2bbb4d91
Akshadachobhee <akshadachobhe7@gmail.com>
2023-07-14 00:09:11 +0530

    Create program255.c

---
+++ C LB/program255.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+    int no;
+    struct Node *next;
+} NODE ,*PNODE,**PPNODE ;
+
+/*typedef struct Node NODE;
+typedef struct Node * PNODE;
+typedef struct Node ** PPNODE;
+*/
+
+int main()
+{
+    PNODE First = NULL;
+    
+
+    return 0;
+}
