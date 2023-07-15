6585cb9aa759b7f6e0b4310049789a4edeeb0f4f
Akshadachobhee <akshadachobhe7@gmail.com>
2023-07-15 09:32:07 +0530

    Create program98.c

---
+++ C LB/program98.c
@@ -0,0 +1,27 @@
+/*
+Input : 7
+Output : 0 1 2 3 4 5 
+
+Input : 3
+Output :  0 1 2 
+*/
+#include<stdio.h>
+ void Display(int iFreq)
+ {  
+    int iCnt = 0;
+    for(iCnt = 0; iCnt < iFreq; iCnt++)
+    {
+    printf("%d\t",iCnt); // becauase we don't declare iCnt = 0 except in array 
+    }
+    printf("\n"); // optional
+ } 
+ int main()
+ {
+      int iFrequency = 0;
+    printf("Enter the frequency of symbol");
+    scanf("%d",&iFrequency);
+ 
+    Display(iFrequency);
+
+    return 0;
+ }
